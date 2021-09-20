cb6ef16ea7ca4ae995d106618d6aae1528540ac8
Пользователь <@->
2021-09-20 13:03:09 +0500

    Добавьте файлы проекта.

---
+++ Проекты/Стихотворение/Стихотворение/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Стихотворение
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Ты и Вы");
+            Consolr.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Пустое вы сердечным ты");
+            Console.WriteLine("Она, обмолвясь, заменила");
+            Console.WriteLine("И все счастливые мечты");
+            Console.WriteLine("В душе влюбленной возбудила.");
+            Console.WriteLine("Пред ней задумчиво стою,");
+            Console.WriteLine("Свести очей с нее нет силы;");
+            Console.WriteLine("И говорю ей: как вы милы!");
+            Console.WriteLine("И мыслю: как тебя люблю!");
+        }
+    }
+}
