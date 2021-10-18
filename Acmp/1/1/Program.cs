ac997b2f17b593a4b8e731180528ea02c640534b
Elizaveta.Mamzeleva@urfu.me <Elizaveta.Mamzeleva@urfu.me>
2021-10-18 13:36:08 +0500

    Функция

---
+++ Acmp/1/1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToInt32(Console.ReadLine());
+            double c = a + b;
+            Console.WriteLine(c);
+            
+        }
+    }
+}
---
+++ Программирование/Проекты/1. Дом.работы/Новая папка/ВыделениеМетода/ВыделениеМетода/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ВыделениеМетода
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x= ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Math.Sqrt(x * x + Math.Sqrt(x * x + 1 / x * x));
+            Console.WriteLine("Y={0} ", y);
+        }
+    }
+}
